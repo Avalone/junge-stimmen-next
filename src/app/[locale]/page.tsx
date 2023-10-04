956df3eaadbfd3dc48d16da2dcdc87c54584d1f1
Anatoly Ivanov <avalone@outlook.com>
2023-10-04 11:34:15 +0300

    Minor improvements

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,7 +6,7 @@ export default function IndexPage() {
   const t = useTranslations();
 
   return (
-    <main className="pt-6 pb-6 grid grid-cols-2 grid-rows-5 gap-2">
+    <main className="pt-6 pb-6 grid grid-cols-2 grid-rows-4 gap-2">
       <div>
         <header className="main-header">
           <h1>{t("title")}</h1>
@@ -16,7 +16,9 @@ export default function IndexPage() {
         <div className="announce-badge bg-cyan-50 rounded-[55px] m-5 p-5 text-center">
           <h1 className="">25.12.2022</h1>
           <h2 className="mt-4">{t("announce-text")}</h2>
-          <button className="mt-4 p-4 bg-cyan-400 bg-opacity-70 rounded-3xl items-center gap-2.5 text-3xl">Смотреть подробнее</button>
+          <button className="mt-4 p-4 bg-cyan-400 bg-opacity-70 rounded-3xl items-center gap-2.5 text-3xl">
+            Смотреть подробнее
+          </button>
         </div>
       </div>
       <picture>
@@ -27,17 +29,32 @@ export default function IndexPage() {
           height={907}
         />
       </picture>
-      <article>
-        {t("choirDescription")}
-      </article>
+
+      <article>{t("choirDescription")}</article>
       <div>
-      <Image
+        <Image
           src="/video_mock.png"
           className="rounded-[20px]"
           alt={t("title")}
           width={623}
           height={507}
         />
+      </div>
+
+      <div>
+        <h2>{t("directorTitle")}</h2>
+        <article>{t("directorDescription")}</article>
+      </div>
+      <picture>
+        <Image
+          src="/elena_vasil_1.png"
+          width={499}
+          height={644}
+          alt={t("directorTitle")}
+        />
+      </picture>
+      <div>
+        <p>{t("audioDescription")}</p>
       </div>
     </main>
   );
