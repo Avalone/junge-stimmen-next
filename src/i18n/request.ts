0d48155a8df9eb1532b0b8f82d3601f564b03d0e
Anatoly Ivanov <anatoly.ivanov@tvbet.tv>
2025-04-15 19:42:57 +0300

    fix: build error fixed by making routes to messages static

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,11 +1,28 @@
 import {getRequestConfig} from 'next-intl/server';
 import {routing} from './routing';
 
+// async function importMessages(locale: string) {
+//     const prefix = `../messages/${locale}`;
+//     return {
+//         'common': (await import(`${prefix}/common.json`)).default,
+//         'home': (await import(`${prefix}/home.json`)).default
+//     }
+// }
+
 async function importMessages(locale: string) {
-    const prefix = `../../messages/${locale}`;
-    return {
-        'common': (await import(`${prefix}/common.json`)).default,
-        'home': (await import(`${prefix}/home.json`)).default
+    switch (locale) {
+        case 'ru':
+            return {
+                'common': (await import('../messages/ru/common.json')).default,
+                'home': (await import('../messages/ru/home.json')).default
+            };
+        case 'en':
+            return {
+                'common': (await import('../messages/en/common.json')).default,
+                'home': (await import('../messages/en/home.json')).default
+            };
+        default:
+            throw new Error(`Unknown locale: ${locale}`);
     }
 }
 
