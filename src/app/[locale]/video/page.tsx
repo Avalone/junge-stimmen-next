7e868cd44d48420ce14c5b64aeca028e3840af76
Anatoly Ivanov <anatoly.ivanov@tvbet.tv>
2025-06-22 12:11:03 +0300

    feat: added grid to video list

--- src/app/[locale]/video/page.tsx
+++ src/app/[locale]/video/page.tsx
@@ -11,14 +11,21 @@ export default async function VideoPage() {
         <>
             <main>
                 <h2>Видеозаписи</h2>
-                {
-                    videosInfo.map((info, index) => (
-                        <div key={index}>
-                            <YoutubePreview videoId={info.videoId} title={info.title}
-                                            className="my-4 max-w-3xl mx-auto"/>
-
-                        </div>))
-                }
+                <div className="mt-8 grid gap-6
+                      grid-cols-1
+                      sm:grid-cols-2
+                      md:grid-cols-2
+                      lg:grid-cols-3">
+                    {videosInfo.map((info, index)=> (
+                        <article key={index}
+                                 className="bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-shadow">
+                            <YoutubePreview videoId={info.videoId} className="w-full aspect-video object-cover"/>
+                            <div className="p-4">
+                                {info.title}
+                            </div>
+                        </article>
+                    ))}
+                </div>
             </main>
         </>
     );
